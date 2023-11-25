ece74cdfca61b6713e42135802f6b8610de5f83d
immamm <imamdurosinlohun@gmail.com>
2023-11-25 06:44:24 +0100

    fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,71 +1,3 @@
-// 'use client';
-
-// import { motion } from 'framer-motion';
-// import { socials } from '../constants';
-
-// import styles from '../styles';
-// import { footerVariants } from '../utils/motion';
-
-// const Footer = () => (
-//   <motion.footer
-//     variants={footerVariants}
-//     initial="hidden"
-//     whileInView="show"
-//     className={`${styles.xPaddings} py-8 relative`}
-//   >
-//     <div className="footer-gradient" />
-//     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-//       <div className="flex items-center justify-between flex-wrap gap-5">
-//         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-//           Start the Junglequest
-//         </h4>
-//         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-//           <img
-//             src="/headset.svg"
-//             alt="headset"
-//             className="w-[24px] h-[24px] object-contain"
-//           />
-//           <span className="font-normal text-[16px] text-white">
-//             Enjoy your quest
-//           </span>
-//         </button>
-//       </div>
-
-//       <div className="flex flex-col">
-//         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
-//         <div className="flex items-center justify-between flex-wrap gap-4">
-//           <h4 className="font-extrabold text-[24px] text-white">
-//             JUNGLEQUEST
-//           </h4>
-//           <p className="font-normal text-[14px] text-white opacity-50">
-//             Copyright © 2023 JunglequestNFT.
-//           </p>
-
-//           <div className="flex gap-4">
-//             {socials.map((social) => (
-//               <a
-//                 key={social.name}
-//                 href={social.url}
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <img
-//                   src={social.icon}
-//                   alt={social.name}
-//                   className="w-[24px] h-[24px] object-contain cursor-pointer"
-//                 />
-//               </a>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </motion.footer>
-// );
-
-// export default Footer;
-
 'use client';
 
 import { motion } from 'framer-motion';
@@ -75,11 +7,7 @@ import { footerVariants } from '../utils/motion';
 
 const Footer = () => {
 
-  // const handleButtonClick = () => {
-  //   alert('Quest coming soon');
-  // };
-
-  return(
+  return (
     <motion.footer
     variants={footerVariants}
     initial="hidden"
@@ -143,6 +71,4 @@ const Footer = () => {
   )
 }
 
-
 export default Footer;
-
